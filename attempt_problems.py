82f8a6dfd2b33e0bdafde74e116840a7d528cd84
Curt Cox <curtcox@gmail.com>
2024-10-26 02:25:20 +0000

    Finding invariants.

--- attempt_problems.py
+++ attempt_problems.py
@@ -9,11 +9,39 @@ def load_problems(directory) -> dict:
                 problems[filename] = json.load(file)
     return problems
 
+def construct_output(input, invariants):
+    return input
+
+def is_length_equal(train) -> bool:
+    for example in train:
+        if len(example['input']) != len(example['output']):
+            return False
+    return True
+
+def is_length_less_than(train) -> bool:
+    for example in train:
+        if len(example['input']) >= len(example['output']):
+            return False
+    return True
+
+def is_length_greater_than(train) -> bool:
+    for example in train:
+        if len(example['input']) <= len(example['output']):
+            return False
+    return True
+
+def find_invariants(problem) -> list:
+    invariants = {}
+    train = problem['train']
+    invariants['length_equal']        = is_length_equal(train)
+    invariants['length_greater_than'] = is_length_greater_than(train)
+    invariants['length_less_than']    = is_length_less_than(train)
+    print(invariants)
+    return invariants
+
 def guess_output(problem) -> list:
-    # for pair in problem['train']:
-    #     consider pair['input'] produces pair['output']:
     for pair in problem['test']:
-        return pair['input']
+        return construct_output(pair['input'], find_invariants(problem))
 
 def attempt_problem(name, problem) -> bool:
     print(f"Attempting : {name}")
