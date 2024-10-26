1466288305c3e4305eb657ad0e08e8cd8a800065
Curt Cox <curtcox@gmail.com>
2024-10-26 01:06:16 +0000

    Add output

--- attempt_problems.py
+++ attempt_problems.py
@@ -21,19 +21,21 @@ def attempt_problem(problem):
 
 def main():
 
-    total_successes = 0
+    successes = 0
 
     training_problems = load_problems('data/training')
     for problem in training_problems:
         if attempt_problem(problem):
-          total_successes += 1
+          successes += 1
+
+    print(f"Training successes: {successes}")
 
     evaluation_problems = load_problems('data/evaluation')
     for problem in evaluation_problems:
         if attempt_problem(problem):
-           total_successes += 1
+           successes += 1
 
-    print(f"Total number of successes: {total_successes}")
+    print(f"Total successes: {successes}")
 
 if __name__ == "__main__":
     main()
